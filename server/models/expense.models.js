7535a12da302e319687def4ecabab0e73aabbbcd
W. Vanegas <wvanegas405@gmail.com>
2023-01-03 19:04:29 -0500

    Update model

--- server/models/expense.models.js
+++ server/models/expense.models.js
@@ -2,8 +2,13 @@ import {Schema, model} from 'mongoose';
 
 const expenseSchema = new Schema({
     budget: {
-        type: Array,
+        type: Object,
         required: false
+    },
+    balance: {
+        type: Number,
+        required: false,
+        default: 0
     },
     expenses: {
         type: Array,
